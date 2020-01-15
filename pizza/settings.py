43db27c726e21e287f05631e587117191df2f901
Prashant Singh <psonlinux@gmail.com>
2020-01-15 19:00:44 +0530

    update settings.py

--- pizza/settings.py
+++ pizza/settings.py
@@ -117,9 +117,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # Send emails to pizza customer
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
